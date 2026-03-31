454ee888b8750d2ca847896bf90ef1b038fe0c2f
Fermoal <105529956+Fermoal@users.noreply.github.com>
2026-03-30 18:05:43 -0600

    docs: add credentials.example.ts new version

--- tests/fixtures/credentials.example.ts
+++ tests/fixtures/credentials.example.ts
@@ -2,8 +2,8 @@
 // cp tests/fixtures/credentials.example.ts tests/fixtures/credentials.ts
 
 export const TEST_USER = {
-  email: 'your-email@example.com',
-  password: 'your-password',
+  email: process.env.TEST_EMAIL || 'fmalvarado81@gmail.com',
+  password: process.env.TEST_PASSWORD || 'fLS2a@?65OFu',
 };
 
 export const BASE_URL = 'https://atlas.atlasdevel5.com';
